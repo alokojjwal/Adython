fd49e8fbb31df1c01ad5594a0f646902e3bd45dd
Alokojjwal <90019775+alokojjwal@users.noreply.github.com>
2021-09-24 18:44:41 +0530

    Delete factorial.py

--- factorial.py
+++
@@ -1,8 +0,0 @@
-n=int(input("Enter a positive number to find the factorial of it: "))
-if n<0:
-    print("Not a valid number")
-else:
-    fact=1
-    for i in range(1,n+1):
-        fact=fact*i
-    print("The factorial of",n,"is:",fact)
